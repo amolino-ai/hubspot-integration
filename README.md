faf6f0fed1a63e54f76c3ceabbac50490bdafb50
Karanbir Singh <65090469+karansohi@users.noreply.github.com>
2024-08-13 21:44:29 +0000

    Fix Create and Update Functions: - Add examples. - Load API Key via environment. - Add dockerfile. - Shift to txt based pip installation.

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 # hubspot-integration
 
-Run `pip install hubspot-api-client` for HubSpot SDK. 
+Run `pip install -r requirements.txt` for installing all dependencies.. 
+
+## How to run after installing all tools:
+HUBSPOT_API_KEY=API_KEY uvicorn fast-api-hubspot:app --reload
+## Get Deal
+curl -X GET "http://localhost:8000/deals/Test-Deal"
+
+## Check if deal already exists
+curl -X POST "http://localhost:8000/create-deal" \
+-H "Content-Type: application/json" \
+-d '{
+  "amount": 3000,
+  "closedate": "2024-10-30",
+  "dealname": "fast-api-deal",
+  "dealstage": "contractsent",
+  "last_updated": "2024-08-07"
+}'
+
+## Create new deal
+curl -X POST "http://localhost:8000/create-deal" \
+-H "Content-Type: application/json" \
+-d '{
+  "amount": 3000,
+  "closedate": "2024-10-30",
+  "dealname": "fast-api-deal",
+  "dealstage": "contractsent",
+  "last_updated": "2024-08-07"
+}'
+
+## Update a deal
+curl -X PUT "http://localhost:8000/update-deal" \
+-H "Content-Type: application/json" \
+-d '{
+  "name": "Test-Deal",
+  "amount": 10000,
+  "last_updated": "2024-08-07"
+}'
+
+## Update a deal that already has the latest info
+curl -X PUT "http://localhost:8000/update-deal" \
+-H "Content-Type: application/json" \
+-d '{
+  "name": "Test-Deal",
+  "amount": 10000,
+  "last_updated": "2024-08-07"}'
--- fast-api-hubspot.py
+++ fast-api-hubspot.py
@@ -1,14 +1,22 @@
+import os
 from fastapi import FastAPI, HTTPException
 from hubspot import HubSpot
 from hubspot.crm.deals import SimplePublicObjectInput, SimplePublicObjectInputForCreate
 from pydantic import BaseModel
-import os
 from datetime import datetime, date
+from typing import Optional
+from dateutil import parser
+from pytz import UTC
 
 app = FastAPI()
 
+# Load API key from environment variable
+api_key = os.getenv('HUBSPOT_API_KEY')
+if not api_key:
+    raise ValueError("HUBSPOT_API_KEY environment variable is not set")
+
 # Initialize HubSpot client
-# api_client = HubSpot(access_token='')
+api_client = HubSpot(access_token=api_key)
 
 class DealCreate(BaseModel):
     amount: float
@@ -18,10 +26,11 @@ class DealCreate(BaseModel):
     last_updated: date
 
 class DealUpdate(BaseModel):
-    id: str
+    name: str
     amount: float
     last_updated: date
 
+# Send a get request to this url
 @app.get("/deals/{deal_name}")
 def find_deal_by_name(deal_name: str):
     try:
@@ -38,12 +47,21 @@ def find_deal_by_name(deal_name: str):
         if api_response.results:
             return api_response.results[0].to_dict()
         else:
-            raise HTTPException(status_code=404, detail="Deal not found")
+            return None
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
 
-@app.post("/deals")
+# Create deals by sending new deal properties to this url
+@app.post("/create-deal")
 def create_new_deal(deal: DealCreate):
+    try:
+        existing_deal = find_deal_by_name(deal.dealname)
+        if existing_deal:
+            raise HTTPException(status_code=400, detail="Deal already exists")
+    except HTTPException as e:
+        if e.status_code != 404:
+            raise e
+
     properties = deal.dict()
     properties.pop("last_updated", None)
     try:
@@ -53,20 +71,30 @@ def create_new_deal(deal: DealCreate):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred while creating a new deal: {str(e)}")
 
-@app.put("/deals")
+# Update deals by sending latest property values to this url
+@app.put("/update-deal")
 def update_deal(deal: DealUpdate):
     try:
-        existing_deal = api_client.crm.deals.basic_api.get_by_id(deal_id=deal.id)
-        hs_lastmodified = datetime.fromisoformat(existing_deal.properties['hs_lastmodifieddate'].replace('Z', '+00:00'))
-        last_updated = deal.last_updated.replace(tzinfo=hs_lastmodified.tzinfo)
+        existing_deal = find_deal_by_name(deal.name)
+        if existing_deal is None:
+            raise HTTPException(status_code=404, detail="Deal not found")
+
+        hs_lastmodified_str = existing_deal['properties'].get('hs_lastmodifieddate')
+        if not hs_lastmodified_str:
+            raise HTTPException(status_code=500, detail="hs_lastmodifieddate not found in deal properties")
+
+        hs_lastmodified = parser.isoparse(hs_lastmodified_str).replace(tzinfo=UTC)
+        last_updated = datetime.combine(deal.last_updated, datetime.min.time()).replace(tzinfo=UTC)
 
         if last_updated > hs_lastmodified:
             properties = {"amount": str(deal.amount)}
             simple_public_object_input = SimplePublicObjectInput(properties=properties)
-            response = api_client.crm.deals.basic_api.update(deal_id=deal.id, simple_public_object_input=simple_public_object_input)
+            response = api_client.crm.deals.basic_api.update(deal_id=existing_deal['id'], simple_public_object_input=simple_public_object_input)
             return response.to_dict()
         else:
-            return {"message": "Deal already contains latest information"}
+            return {"message": "Deal already contains the latest information"}
+    except HTTPException as e:
+        raise e
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred while updating the deal: {str(e)}")
 
